e1147fe98e04bfc931783aef518a97f4a91494a1
mochenya <2827075398@qq.com>
2024-01-10 16:12:23 +0800

    add eslint add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "@/assets/styles/tailwind.css";
+import "@/assets/styles/style.less";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
