0576c2b4bff3a3d289a2f4594100d5bdff294ca3
mochenya <2827075398@qq.com>
2024-01-13 18:02:50 +0800

    clean code

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,23 +1,9 @@
 <script lang="ts" setup>
-import {onMounted, ref} from "vue";
+import {ref} from "vue";
 import LoginContainer from "@/components/login/LoginContainer.vue";
 import SignUpContainer from "@/components/login/SignUpContainer.vue";
+import bgImage from "@/assets/images/login_bg.png";
 
-onMounted(() => {
-  setLoginPageContainerBackground()
-      .catch((error) => {
-        console.error(error);
-      });
-});
-const loginPageBackgroundRef = ref<HTMLElement | null>(null);
-const wallpaper = "https://images.unsplash.com/photo-1704574155820-47a04256005d?w=600";
-const setLoginPageContainerBackground = async () => {
-  // const bingWallpaper = await getBingWallpaper();
-  if (!loginPageBackgroundRef.value) {
-    throw new Error("loginPageContainer is null");
-  }
-  loginPageBackgroundRef.value.style.backgroundImage = `url(${wallpaper})`;
-};
 const isLogin = ref(true);
 
 </script>
@@ -25,9 +11,8 @@ const isLogin = ref(true);
 <template>
   <div class="w-full h-full flex flex-row gap-0.5 bg-neutral-50">
     <div
-        ref="loginPageBackgroundRef"
         class="flex-2 bg-cover bg-no-repeat bg-center rounded-r-xl"
-        style="background-image: url('../assets/images/login_bg.png')"
+        :style="{backgroundImage: `url(${bgImage})`}"
     />
     <div class="flex-1 h-full flex flex-col justify-center">
       <div class="w-96 mx-auto">
